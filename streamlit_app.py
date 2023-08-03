2a7308fa510e7dacd23d648ac3ea8f5ee1158260
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-03 18:20:44 +0530

    Update streamlit_app.py
    Text added to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 
 st.title('My parents New Healthy Diner')
-st.divider()
-st.header('Breakfast Menu')
-st.divider()
-st.write('Omega 3 and Blueberry Oatmeal')
-st.write('Kale Spinach and Roket Smoothie')
-st.write('Hard Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
