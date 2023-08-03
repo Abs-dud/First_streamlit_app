bc8269f026b7a15b417c11c354ebd0b341f54ad0
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-03 18:18:05 +0530

    Update streamlit_app.py
    Adding test to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.title('My parents New Healthy Diner')
+st.devider()
+st.header('Breakfast Menu')
+st.devider()
+st.write('Omega 3 and Blueberry Oatmeal')
+st.write('Kale Spinach and Roket Smoothie')
+st.write('Hard Boiled Free-Range Egg')
 
 
