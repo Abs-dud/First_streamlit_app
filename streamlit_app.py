07c23c2132278a4b1c831d2f943a0a41b4260594
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-03 22:16:34 +0530

    Update streamlit_app.py
    Adding pandas to read from amazon s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.title('My parents New Healthy Diner')
 st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -8,4 +8,6 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avacado Tost')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
 
