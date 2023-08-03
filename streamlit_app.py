444f66b6b82d99ab67375bf0a0ed71e398efa227
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-03 18:19:16 +0530

    Update streamlit_app.py
    Error corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 st.title('My parents New Healthy Diner')
-st.devider()
+st.divider()
 st.header('Breakfast Menu')
-st.devider()
+st.divider()
 st.write('Omega 3 and Blueberry Oatmeal')
 st.write('Kale Spinach and Roket Smoothie')
 st.write('Hard Boiled Free-Range Egg')
