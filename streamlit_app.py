48f73b48f0492d392f33acc1a2d4adadec3b2876
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-04 17:02:42 +0530

    Update streamlit_app.py
    type error fixed for multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Strawberries'])
-fruit_to_show = fruit_selected.loc[fruit_selected]
+fruit_to_show = my_fruit_list.loc[fruit_selected]
 st.dataframe(fruit_to_show)
 
 
