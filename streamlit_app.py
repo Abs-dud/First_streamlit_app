654600953c6e9780f801b0d319103e2dd4627922
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-04 17:01:18 +0530

    Update streamlit_app.py
     show the ones shown in the pick list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ st.text('🥑🍞 Avacado Tost')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Strawberries'])
-st.dataframe(my_fruit_list)
+fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Strawberries'])
+fruit_to_show = fruit_selected.loc[fruit_selected]
+st.dataframe(fruit_to_show)
 
 
