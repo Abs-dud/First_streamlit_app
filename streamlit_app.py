6d7470ed0f5c8af541920188f98ba81d13d9f98a
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-05 16:27:05 +0530

    Update streamlit_app.py
    using variables to automate the fruitname

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ st.dataframe(fruit_to_show)
 
 # new section to see Fruityvice response
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #take a json version of response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
