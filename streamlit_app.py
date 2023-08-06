8c9bfe93e4812313fc715e113c4eeeea7449b7b2
Abs-dud <141332020+Abs-dud@users.noreply.github.com>
2023-08-06 22:58:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,6 @@ add_my_fruit = st.text_input('What fruit would you like to add?')
 if st.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   back_from_function = insert_row_snowflake (add_my_fruit)
-st.write(back_from_function)
+st.text(back_from_function)
 
 
